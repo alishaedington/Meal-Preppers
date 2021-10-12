680fcc09818da73dd7959f060303c5478882003f
Alisha Edington <alisha_56789@yahoo.com>
2021-10-12 11:26:44 -0600

    changed API to heroku dev variable in helper.js

--- backend/server/helpers.js
+++ backend/server/helpers.js
@@ -1,16 +1,16 @@
 const axios = require('axios');
-const auth = require('../../config');
+// const auth = require('../../config');
 
 const getCategories = (request, response) => {
   axios
-    .get(`http://www.themealdb.com/api/json/v2/${auth.API_KEY}/list.php?c=list`)
+    .get(`http://www.themealdb.com/api/json/v2/${process.env.API_KEY}/list.php?c=list`)
     .then((results) => response.send(results.data))
     .catch((err) => console.log(err));
 };
 
 const getCuisineTypes = (request, response) => {
   axios
-    .get(`http://www.themealdb.com/api/json/v2/${auth.API_KEY}/list.php?a=list`)
+    .get(`http://www.themealdb.com/api/json/v2/${process.env.API_KEY}/list.php?a=list`)
     .then((results) => response.send(results.data))
     .catch((err) => console.log(err));
 };
@@ -18,7 +18,7 @@ const getCuisineTypes = (request, response) => {
 const filterByCategory = (request, response) => {
   const { category } = request.query;
   axios
-    .get(`http://www.themealdb.com/api/json/v2/${auth.API_KEY}/filter.php?c=${category}`)
+    .get(`http://www.themealdb.com/api/json/v2/${process.env.API_KEY}/filter.php?c=${category}`)
     .then((results) => response.send(results.data))
     .catch((err) => console.log(err));
 };
@@ -26,14 +26,14 @@ const filterByCategory = (request, response) => {
 const filterByCuisine = (request, response) => {
   const { cuisine } = request.query;
   axios
-    .get(`http://www.themealdb.com/api/json/v2/${auth.API_KEY}/filter.php?a=${cuisine}`)
+    .get(`http://www.themealdb.com/api/json/v2/${process.env.API_KEY}/filter.php?a=${cuisine}`)
     .then((results) => response.send(results.data))
     .catch((err) => console.log(err));
 };
 
 const getLatest = (request, response) => {
   axios
-    .get(`http://www.themealdb.com/api/json/v2/${auth.API_KEY}/latest.php`)
+    .get(`http://www.themealdb.com/api/json/v2/${process.env.API_KEY}/latest.php`)
     .then((results) => response.send(results.data))
     .catch((err) => console.log(err));
 };
@@ -41,14 +41,14 @@ const getLatest = (request, response) => {
 const getRecipe = (request, response) => {
   const { mealId } = request.query;
   axios
-    .get(`http://www.themealdb.com/api/json/v2/${auth.API_KEY}/lookup.php?i=${mealId}`)
+    .get(`http://www.themealdb.com/api/json/v2/${process.env.API_KEY}/lookup.php?i=${mealId}`)
     .then((results) => response.send(results.data))
     .catch((err) => console.log(err));
 };
 
 const getRandomRecipe = (request, response) => {
   axios
-    .get(`http://www.themealdb.com/api/json/v2/${auth.API_KEY}/random.php`)
+    .get(`http://www.themealdb.com/api/json/v2/${process.env.API_KEY}/random.php`)
     .then((results) => response.send(results.data))
     .catch((err) => console.log(err));
 };
